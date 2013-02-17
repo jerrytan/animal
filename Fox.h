608a2c3cec20ffc1f643c8ce0dd8b7c97e330434
jerrytan <zhongyi.tan@gmail.com>
2013-02-17 14:48:11 +0800

    add Fox implementation

---
+++ Fox.h
@@ -0,0 +1,13 @@
+//
+//  Fox.h
+//  DesignPattern
+//
+//  Created by tan zhongyi on 13-2-17.
+//  Copyright (c) 2013年 Sanny. All rights reserved.
+//
+
+#import "Animal.h"
+
+@interface Fox: Animal
+
+@end
---
+++ Fox.m
@@ -0,0 +1,22 @@
+//
+//  Fox.m
+//  DesignPattern
+//
+//  Created by tan zhongyi on 13-2-17.
+//  Copyright (c) 2013年 Sanny. All rights reserved.
+//
+
+#import "Fox.h"
+
+@implementation Fox
+
+-(id) init {
+    
+    if (self = [super init]) {
+        name = @"Fox";
+    }
+    return self;
+    
+
+}
+@end
